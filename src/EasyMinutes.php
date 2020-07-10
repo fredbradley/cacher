8349f136a3bd387964c1faa636b4aecbd79a1b9d
Fred Bradley <hello@fredbradley.co.uk>
2020-07-10 14:02:28 +0100

    Add Easy Minutes Class

---
+++ src/EasyMinutes.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace FredBradley\Cacher;
+
+
+/**
+ * Class EasyMinutes
+ * @package FredBradley\Cacher
+ */
+class EasyMinutes
+{
+    public const AN_HOUR = 60;
+    public const A_DAY = 24 * self::AN_HOUR;
+    public const A_WEEK = 7 * self::A_DAY;
+    public const A_MONTH = 30 * self::A_DAY;
+    public const A_YEAR = 365 * self::A_DAY;
+
+}
